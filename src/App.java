592d23e5828b5dbf7e9b79467a0564ba90fcbdf5
GuillaumeChamp <guillaume.champtoussel@ensea.fr>
2021-10-12 11:05:27 +0200

    Implementing two more buttons Follow the TODO to end the project

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
-import java.util.ArrayList;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.canvas.*;
@@ -9,19 +11,26 @@ import javax.sound.sampled.LineUnavailableException;
 public class App extends Application {
     Group root = new Group();
     SoundLoop microphone;
+    Canvas canvas;
+    final long width = 500;
+    final long height = 800;
 
     public void start(Stage theStage) throws LineUnavailableException {
         theStage.setTitle("Vocoder");
-        ArrayList<String> input = new ArrayList<>(); //store the keyboard input
-        final long width = 512; //width of the window
-        final long height = 512; //height of the window
+
         microphone = new SoundLoop();
         microphone.run();
 
-        Canvas canvas = new Canvas(width, height);
+        canvas = new Canvas(width, height);
         root.getChildren().add(canvas);
         DefineButton();
         Scene scene = new Scene(root,width,height);
+
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
+                Draw();
+            }
+        }.start();
         theStage.setScene(scene);
         theStage.show();
     }
@@ -33,14 +42,35 @@ public class App extends Application {
             }
             else microphone.play();
         });
-        play.setTranslateX(100);
-        play.setTranslateY(100);
+        play.setTranslateY(400);
+        play.setTranslateX(250);
         play.setVisible(true);
         play.setVisible(true);
         root.getChildren().add(play);
+
+        Button button= new Button("echo");
+        button.setOnAction(e-> microphone.addEcho());
+        button.setTranslateY(200);
+        button.setTranslateX(200);
+        root.getChildren().add(button);
+
+        button= new Button("distortion");
+        button.setOnAction(e-> microphone.addDistortion());
+        button.setTranslateY(200);
+        button.setTranslateX(300);
+        root.getChildren().add(button);
     }
     //Todo work on the close option
-    //speaker.close();
-    //microphone.close();
+    private void Close(){
+        microphone.Close();
+    }
+    private void Draw(){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.drawImage(new Image("JavaLogo.png",width,height,true,true),50,0);
+        //Todo : change picture to allow override of the previous state
+        gc.setFill(Color.BLACK);
+        gc.fillText("Echo : " + microphone.isEcho(),400,700);
+        gc.fillText("Distortion : " + microphone.isDistortion(),400,710);
+    }
 }
 
--- src/SoundLoop.java
+++ src/SoundLoop.java
@@ -4,9 +4,11 @@ import java.io.ByteArrayOutputStream;
 public class SoundLoop implements Runnable {
     AudioFormat format = new AudioFormat( 8000.0f,16, 1,true, true);
     //Format propre au micro du PC
-    TargetDataLine microphone;
-    SourceDataLine speaker;
+    private final TargetDataLine microphone;
+    private final SourceDataLine speaker;
     boolean active = false;
+    private boolean echo = false;
+    private boolean distortion = false;
 
     public SoundLoop() throws LineUnavailableException {
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
@@ -14,9 +16,28 @@ public class SoundLoop implements Runnable {
         DataLine.Info speakerInfo = new DataLine.Info(SourceDataLine.class, format);
         speaker = (SourceDataLine) AudioSystem.getLine(speakerInfo);
     }
+    public void addEcho(){
+        /*if (!echo)control.setValue(2);
+        else control.setValue(0);
+        Disable if reverb not supported
+         */
+        echo=!echo;
+    }
+
+    public boolean isEcho() {
+        return echo;
+    }
+
+    public boolean isDistortion() {
+        return distortion;
+    }
+
+    public void addDistortion(){
+        distortion=!distortion;
+    }
 
     public void stop(){
-        //TODO : build a stop option
+        //TODO : build a stop option have a look at IT
         active=false;
     }
     public void play(){
@@ -26,11 +47,22 @@ public class SoundLoop implements Runnable {
         int sampleRate = 1024;
         byte[] rawData = new byte[microphone.getBufferSize()/5];
 
-        while (numBytes<100000){
+        long uptime = 100000;
+        while (numBytes< uptime){
             int numberOfByteRead = microphone.read(rawData,0,sampleRate);
             numBytes += numberOfByteRead;
-            outputData.write(rawData,0,numberOfByteRead);
+            //Todo : change the rawData by rawData*amp
+            //Todo : add a limit option
+            outputData.write(rawData,0,numberOfByteRead/2);
             speaker.write(rawData,0,numberOfByteRead);
+            if (numBytes>1000 & echo){
+                try {
+                    speaker.write(rawData, numberOfByteRead/2-1, numberOfByteRead/2);
+                }catch (IndexOutOfBoundsException e){
+                    e.printStackTrace();
+                    System.out.println("Echo : Fix me");
+                }
+            }
         }
         active=false;
         speaker.drain(); //clear data remain in the speaker buffer
@@ -40,6 +72,16 @@ public class SoundLoop implements Runnable {
     public void run() {
         try {
             microphone.open(format); //Equivalent to init
+            /*
+            try {
+                control = (FloatControl) speaker.getControl(FloatControl.Type.REVERB_SEND);
+            }catch (Exception e){
+                e.printStackTrace();
+                System.out.println("Reverb not supported ");
+            }
+
+             */
+
             microphone.start();
             speaker.open(format);
             speaker.start();
@@ -49,12 +91,16 @@ public class SoundLoop implements Runnable {
             System.out.println("Unable to read");
         }
     }
+    public void Close(){
+        speaker.drain();
+        speaker.close();
+        microphone.close();
+    }
 
     public static void main(String[] args) throws LineUnavailableException {
             SoundLoop sound = new SoundLoop();
             sound.run();
             sound.play();
-
         }
     }
 
